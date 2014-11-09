b9eb04d1d550c7c9306230a1ee5319a72a38b9fc
mura-s <seiya.muramatsu1@gmail.com>
2014-11-09 20:30:19 +0900

    /へのgetに対応。設定ファイルをパースしてviewを作って返す。

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'haml'
+require 'yaml'
 
 module Questionnaire
   class App < Sinatra::Base
@@ -8,8 +9,28 @@ module Questionnaire
       register Sinatra::Reloader
     end
 
+    configure do
+      config = YAML.load_file('questionnaire.yml')
+      set title: config['title']
+      set questions: config['questions']
+    end
+
     get '/' do
+      init_default_vals
       haml :index
+    end
+
+    post '/' do
+      init_default_vals
+
+      haml :close
+    end
+
+    private
+
+    def init_default_vals
+      @title = settings.title
+      @questions = settings.questions.values
     end
   end
 end
